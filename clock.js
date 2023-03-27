a6513149bbe188c85c079eaa9bc15df40d2347c3
Minseok Chae <117037249+chaeminseok@users.noreply.github.com>
2023-03-28 00:52:50 +0900

    makeClock

---
+++ clock.js
@@ -0,0 +1,14 @@
+const clock=document.querySelector(".live-clock span");
+
+
+function getClock(){
+    const date = new Date();
+    let hours= String(date.getHours());
+    const minutes= String(date.getMinutes()).padStart(2,"0");
+    if (hours==="0"){
+        hours="12";
+    }
+    clock.innerText=`${hours}:${minutes}`;
+}
+getClock();
+setInterval(getClock , 1000);
